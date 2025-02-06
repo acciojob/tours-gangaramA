41d6d56c5357831c4630864e8bc929424493109b
gangaramA <150507677+gangaramA@users.noreply.github.com>
2025-02-06 14:40:59 +0530

    Update Tours.jsx

--- src/components/Tours.jsx
+++ src/components/Tours.jsx
@@ -1,1 +1,15 @@
+import React from 'react';
+import Tour from './Tour';
+
+const Tours = ({ tours, setTours }) => {
+  return (
+    <div className="tours-list">
+      {tours.map(tour => (
+        <Tour key={tour.id} tour={tour} setTours={setTours} />
+      ))}
+    </div>
+  );
+};
+
+export default Tours;
 
